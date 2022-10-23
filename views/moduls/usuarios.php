7f931e382a13b9a8dcda9c696f72355772d5f233
Mario Zelaya <mariozelaya23@gmail.com>
2022-10-23 09:30:53 -0700

    adding button adduser to open a modal window, and added users table

--- views/moduls/usuarios.php
+++ views/moduls/usuarios.php
@@ -9,7 +9,7 @@
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
             <li class="breadcrumb-item active">Administrar Usuarios</li>
           </ol>
         </div>
@@ -23,29 +23,78 @@
     <!-- Default box -->
     <div class="card">
       <div class="card-header">
-        <h3 class="card-title">Title</h3>
-
-        <div class="card-tools">
-          <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-            <i class="fas fa-minus"></i>
-          </button>
-          <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-            <i class="fas fa-times"></i>
-          </button>
-        </div>
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuario">Agregar usuario</button> <!-- Modal means to open a window -->
       </div>
+
       <div class="card-body">
-        Start creating your amazing application!
+        
+        <table id="tablaUsuarios" class="table table-bordered table-striped">
+          
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nombre</th>
+              <th>Usuario</th>
+              <th>Foto</th>
+              <th>Perfil</th>
+              <th>Estado</th>
+              <th>Ultimo Login</th>
+              <th>Acciones</th>
+            </tr>  
+          </thead>
+
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td><button class="btn btn-danger btn-xs">Desactivado</button></td>
+              <td></td>
+              <td>
+                <div class="btn-group">
+                  <button class="btn btn-warning"><i class="fa fa-pen"></i></button>
+                  <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        
+        </table>
+
       </div>
       <!-- /.card-body -->
-      <div class="card-footer">
-        Footer
-      </div>
-      <!-- /.card-footer-->
+
     </div>
     <!-- /.card -->
 
   </section>
   <!-- /.content -->
 </div>
-<!-- /.content-wrapper --> <!-- CONTENT -->
+<!-- /.content-wrapper -->
+
+
+<!-- MODAL ADD USER -->
+<div class="modal fade" id="modalAgregarUsuario">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Large Modal</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      <div class="modal-footer justify-content-between">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
