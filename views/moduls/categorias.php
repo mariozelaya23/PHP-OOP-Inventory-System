175517a84e18e4ae5c4fe863c2424e08d13227bf
Mario zelaya <mariozelaya23@gmail.com>
2022-12-06 13:24:35 -0800

    modifying html of categorias.php

--- views/moduls/categorias.php
+++ views/moduls/categorias.php
@@ -9,7 +9,7 @@
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
             <li class="breadcrumb-item active">Administrar Categorias</li>
           </ol>
         </div>
@@ -23,29 +23,101 @@
     <!-- Default box -->
     <div class="card">
       <div class="card-header">
-        <h3 class="card-title">Title</h3>
-
-        <div class="card-tools">
-          <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-            <i class="fas fa-minus"></i>
-          </button>
-          <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-            <i class="fas fa-times"></i>
-          </button>
-        </div>
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarCategoria">Agregar Categoria</button> <!-- Modal means to open a window -->
       </div>
+
       <div class="card-body">
-        Start creating your amazing application!
+        
+        <table id="tablaUsuarios" class="table table-bordered table-striped tables"> <!-- tables is the class name, I use this name on tamplate.js to activate the DataTable plugin -->
+          
+          <thead>
+            <tr>
+              <th style="width: 10px;">#</th>
+              <th>Categoria</th>
+              <th>Acciones</th>
+            </tr>  
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Equipos electromecanicos</td>
+              <td>
+                <div class="btn-group">
+                  <button class="btn btn-warning"><i class="fa fa-pen"></i></button>
+                  <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        
+        </table>
+
       </div>
       <!-- /.card-body -->
-      <div class="card-footer">
-        Footer
-      </div>
-      <!-- /.card-footer-->
+
     </div>
     <!-- /.card -->
 
   </section>
   <!-- /.content -->
 </div>
-<!-- /.content-wrapper --> <!-- CONTENT -->
+<!-- /.content-wrapper -->
+
+
+<!-- MODAL ADD USER -->
+<div class="modal fade" id="modalAgregarCategoria">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <!-- FORM -->
+      <form role="form" method="post" >
+        <!-- MODAL HEADER -->
+        <div class="modal-header" style="background: #3d9970; color: white;">
+          <h4 class="modal-title">Agregar Categoria</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <!-- MODAL BODY -->
+        <div class="modal-body">
+          <div class="card-body">
+            <!-- Textbox Name -->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-th"></i></span>
+                <input type="text" class="form-control input-lg" name="nuevaCategoria" placeholder="Ingresar Categoria" required>
+              </div>      
+            </div>
+            
+          </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Salir</button>
+          <button type="submit" class="btn btn-primary">Guardar categoria</button>
+        </div>
+      </form> <!-- /FORM -->
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
+
+<script>
+  $(function () {
+    $("#tablaUsuarios").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"],
+    }).buttons().container().appendTo('#tablaUsuarios_wrapper .col-md-6:eq(0)');
+    // $('#example2').DataTable({
+    //   "paging": false,
+    //   "lengthChange": false,
+    //   "searching": false,
+    //   "ordering": true,
+    //   "info": true,
+    //   "autoWidth": false,
+    //   "responsive": true,
+    // });
+  });
+</script>
