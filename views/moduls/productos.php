981061081b34b1558abd52e07f9f93b77d8b269f
Mario zelaya <mariozelaya23@gmail.com>
2023-01-15 14:07:18 -0800

    adding product section and adding product modal form

--- views/moduls/productos.php
+++ views/moduls/productos.php
@@ -9,7 +9,7 @@
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
             <li class="breadcrumb-item active">Administrar Productos</li>
           </ol>
         </div>
@@ -23,29 +23,176 @@
     <!-- Default box -->
     <div class="card">
       <div class="card-header">
-        <h3 class="card-title">Title</h3>
-
-        <div class="card-tools">
-          <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-            <i class="fas fa-minus"></i>
-          </button>
-          <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-            <i class="fas fa-times"></i>
-          </button>
-        </div>
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarProducto">Agregar producto</button> <!-- Modal means to open a window -->
       </div>
+
       <div class="card-body">
-        Start creating your amazing application!
+        
+        <table id="tablaProductos" class="table table-bordered table-striped tables"> <!-- tables is the class name, I use this name on tamplate.js to activate the DataTable plugin -->
+          
+          <thead>
+            <tr>
+              <th style="width: 10px;">#</th>
+              <th>Imagen</th>
+              <th>Codigo</th>
+              <th>Descripcion</th>
+              <th>Categoria</th>
+              <th>Stock</th>
+              <th>Precio de compra</th>
+              <th>Fecha Agregado</th>
+              <th>Acciones</th> 
+            </tr>  
+          </thead>
+
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <div class="btn-group">
+                  <button class="btn btn-warning"><i class="fa fa-pen"></i></button>
+                  <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        
+        </table>
+
       </div>
       <!-- /.card-body -->
-      <div class="card-footer">
-        Footer
-      </div>
-      <!-- /.card-footer-->
+
     </div>
     <!-- /.card -->
 
   </section>
   <!-- /.content -->
 </div>
-<!-- /.content-wrapper --> <!-- CONTENT -->
+<!-- /.content-wrapper -->
+
+
+<!-- MODAL ADD PRODUCT -->
+<div class="modal fade" id="modalAgregarProducto">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <!-- FORM -->
+      <form role="form" method="post" enctype="multipart/form-data">
+        <!-- MODAL HEADER -->
+        <div class="modal-header" style="background: #3d9970; color: white;">
+          <h4 class="modal-title">Agregar producto</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <!-- MODAL BODY -->
+        <div class="modal-body">
+          <div class="card-body">
+            <!-- Textbox Codigo -->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-code"></i></span>
+                <input type="text" class="form-control input-lg" name="nuevoCodigo" placeholder="Ingresar Codigo" required>
+              </div>      
+            </div>
+            <!-- Textbox Descripcion-->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fab fa-product-hunt"></i></span>
+                <input type="text" class="form-control input-lg" name="nuevaDescripcion" placeholder="Ingresar Descripcion" required>
+              </div>      
+            </div>
+            <!-- Select Categoria-->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-th"></i></span>
+                <select class="form-control input-lg" name="nuevaCategoria">
+                  <option value="">Seleccionar categorias</option>
+                  <option value="Taladros">Taladros</option>
+                  <option value="Andamios">Andamios</option>
+                </select>
+              </div>      
+            </div>
+            <!-- Textbox Stock-->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-check"></i></span>
+                <input type="number" class="form-control input-lg" name="nuevoStock" min="0" placeholder="Stock" required>
+              </div>      
+            </div>
+            <!-- Textbox Precio Compra-->
+            <div class="form-group row">
+              <div class="col-sm-6">
+                <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-arrow-up"></i></span>
+                  <input type="text" class="form-control input-lg" name="nuevoPrecioCompra" placeholder="Precio de compra" required>
+                </div>   
+              </div>   
+              <!-- Textbox Precio Venta-->
+              <div class="col-sm-6">
+                <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-arrow-down"></i></span>
+                  <input type="text" class="form-control input-lg" name="nuevoPrecioVenta" placeholder="Precio de Venta" required>
+                </div>
+                <br>
+                <!-- Checkbox Porcentaje-->
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <label>
+                      <input type="checkbox" class="minimal porcentaje" checked >
+                      Utilizar porcentaje
+                    </label>
+                  </div>
+                </div>
+                <!-- Textbox para Porcentaje-->
+                <div class="col-xs-6" style="padding: 0;">
+                  <div class="input-group">
+                    <input type="number" class="form-control input-lg nuevoPorcentaje" min="0" value="40" required>
+                    <span class="input-group-addon"><i class="fa fa-percent"></i></span>
+                  </div>
+                </div>
+              </div>  
+            </div>
+            <!-- Upload picture -->
+            <div class="form-group">
+              <div class="panel">SUBIR IMAGEN</div>
+              <input type="file" id="nuevaFoto" name="nuevaFoto">
+              <p class="help-block">Peso maximo de la foto 5MB</p>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Salir</button>
+          <button type="submit" class="btn btn-primary">Guardar producto</button>
+        </div>
+      </form> <!-- /FORM -->
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
+
+<script>
+  $(function () {
+    $("#tablaProductos").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"],
+    }).buttons().container().appendTo('#tablaUsuarios_wrapper .col-md-6:eq(0)');
+    // $('#example2').DataTable({
+    //   "paging": false,
+    //   "lengthChange": false,
+    //   "searching": false,
+    //   "ordering": true,
+    //   "info": true,
+    //   "autoWidth": false,
+    //   "responsive": true,
+    // });
+  });
+</script>
